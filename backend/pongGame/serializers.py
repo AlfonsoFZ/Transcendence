c8eea2e55c0b9b83e2fc335babc93642898f9697
Ismael <imoro-sa@student.42malaga.com>
2025-02-06 16:58:36 +0100

    f*** subject changed!

--- backend/pongGame/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = User
-		fields = ('id', 'name', 'created_at')
-		read_only_fields = ('created_at',)
