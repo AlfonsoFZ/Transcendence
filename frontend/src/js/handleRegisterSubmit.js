502f1f8639c9d77b84bf48a7f230aac3023076bd
Alfonso <afz78@hotmail.com>
2025-04-06 11:08:57 +0200

    alert constraseñas diferentes en register

--- frontend/src/js/handleRegisterSubmit.js
+++ frontend/src/js/handleRegisterSubmit.js
@@ -14,6 +14,13 @@ export function handleRegisterSubmit(event) {
         const form = event.target;
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
+        console.log("Datos del formulario de registro:", data);
+        console.log("data.password: =", data.password);
+        console.log("data.confirm_password: ", data.confirm_password);
+        if (data.password !== data.confirm_password) {
+            alert("Las contraseñas no coinciden. Por favor, verifica e intenta nuevamente.");
+            return;
+        }
         try {
             const response = yield fetch("https://localhost:8443/back/register_user", {
                 method: "POST",
