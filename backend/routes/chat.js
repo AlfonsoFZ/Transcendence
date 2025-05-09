ea6fbed1c4ad57da0eac4b9722125038cc6319c8
fclaus-g <fclaus-g@student.42malaga.com>
2025-05-09 14:04:10 +0200

    feat: fix date log, send userlist on message

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,28 +1,42 @@
 import { parse } from 'cookie';
 import { extractUserFromToken } from '../auth/token.js';
 
-function createMessageJSON(user, message) {
+function createMessageJSON(user, message, type, usersConnected) {
+
+	// Crear el contenedor con los datos de los usuarios conectados
+	const users = usersConnected.map(user => ({
+		userId: user.id,
+		username: user.username,
+		avatarPath: user.avatarPath,
+	}));
 	return {
-		type: "message",
+		type: type,
 		imagePath: user.avatarPath,
 		username: user.username,
 		message: message,
 		timeStamp: getTimeStamp(),
 		// timeStamp: new Date().toLocaleString(),
-		messageStatus: "Sent!"
+		messageStatus: "Sent!",
+		users: users,
 	}
+
 }
 
 function getTimeStamp() {
 	const now = new Date();
-	const hours = now.getHours().toString().padStart(2, '0');
-	const minutes = now.getMinutes().toString().padStart(2, '0');
-	return `${hours}:${minutes}`;
+	// Usar toLocaleTimeString para obtener la hora local en la zona horaria deseada
+	const timeString = now.toLocaleTimeString('es-ES', {
+		hour: '2-digit',
+		minute: '2-digit',
+		timeZone: 'Europe/Madrid' // Cambia 'Europe/Madrid' por tu zona horaria
+	});
+	return timeString;
 }
 
 export function configureChatRoutes(fastify) {
 
 	const clients = new Map();
+	const usersConnected = [];
 
 	fastify.register(async function (fastify) {
 		fastify.get('/ws/chat', { websocket: true }, async (socket, req) => {
@@ -31,6 +45,7 @@ export function configureChatRoutes(fastify) {
 			const token = cookies.token;
 			const user = await extractUserFromToken(token);
 			clients.set(user.id, socket);
+			usersConnected.push(user);
 
 			socket.on('message', message => {
 				try {
@@ -42,13 +57,18 @@ export function configureChatRoutes(fastify) {
 						data = { type: "message", message: message.toString() };
 					}
 					if (data.type === "handshake") {
-						//console.log("Handshake received:", data);
-						//socket.send(JSON.stringify({ type: "handshake", message: "Welcome to the chat!" }));
+						// MI idea es mandar la lista de ususarios en el handshake que es cuando se inicia la conexion
+						// y actualizarla y enviarla a todos cuando se cierre el socket de un user asi no habria que procesar en cada mensaje
+						// pero no me funciona bien
+						// console.log("Handshake received:", data);
+						// const response = createMessageJSON(user, data.message, data.type, usersConnected);
+						// socket.send(JSON.stringify(response));
 						return;
 					}
 					if (data.type === "message") {
 						console.log("Message received:", data);
-						const response = createMessageJSON(user, data.message);
+						const response = createMessageJSON(user, data.message, data.type, usersConnected);
+						console.log("Response to be sent:", response);
 						for (const [id, client] of clients) {
 							client.send(JSON.stringify(response));
 						}
@@ -60,6 +80,11 @@ export function configureChatRoutes(fastify) {
 
 			socket.on('close', () => {
 				clients.delete(user.id);
+				usersConnected.splice(usersConnected.indexOf(user), 1);// Eliminar el usuario de la lista de usuarios
+				for (const [id, client] of clients) {
+					const response = createMessageJSON(user, `${user.username} has left the chat`, "message", usersConnected);
+					client.send(JSON.stringify(response));
+				}
 				console.log(`Client ${user.username} disconnected`);
 			});
 
